31a6b6589c372e94054b494b9bd06645e06e5a1d
Yury Ghirin <yury.ghirin@gmail.com>
2025-05-12 10:33:45 +0300

    Перед Adding a sitemap to the site

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,9 +1,13 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
+import markdown
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.simple_tag
+
 def total_posts():
     # Фукнция возвращает общее количество постов в блоге
     return Post.published.count()
@@ -13,3 +17,15 @@ def show_latest_posts(count=5):
     # Функция возвращает последние опубликованные посты
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    # Функция возвращает самые комментируемые посты
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    # Функция форматирует текст в Markdown
+    return mark_safe(markdown.markdown(text))
